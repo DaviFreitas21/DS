3d9f4b92a8bb3e7b8cac5618121af819e5d441dd
Davi Freitas <df300409@gmail.com>
2026-03-18 12:17:19 -0300

    novos exercicios

---
+++ Menu.java
@@ -0,0 +1,15 @@
+package prjListaMetodos2;
+
+public class Menu {
+	
+	public static void exibirMenu() {
+		System.out.println("Menu");
+		System.out.println("");
+	}
+	
+	
+	public static void main (String[] args) {
+		
+		exibirMenu();
+	}
+}
